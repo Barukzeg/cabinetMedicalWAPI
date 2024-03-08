6d308671b773e75798bdc892925d39ddb0c0eb2b
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2024-03-08 16:23:27 +0100

    add usager.php for control
    rename usager to usagerFunctions

--- ressources/usager/usager.php
+++ ressources/usager/usager.php
@@ -1,128 +1,98 @@
 <?php
 
+    include 'usagerFunctions.php';
+
     //Connexion
     $server = "localhost";
     $db = "cabinetmedical";
     $login = "root";
     $mdp = "";
 
-    $linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
+    $connexion = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
 
-    function getAllUsager(){
-        $query = $linkpdo->prepare("SELECT * FROM usager;");
-        $query->execute();
-        $matchingData = $query->fetchAll(PDO::FETCH_ASSOC);
-        return $matchingData;
+
+
+    /// Identification du type de méthode HTTP envoyée par le client
+    $http_method = $_SERVER['REQUEST_METHOD'];
+
+    switch ($http_method){
+        case "GET" :
+            //Récupération des données dans l’URL si nécessaire
+            if(isset($_GET['id'])){
+                $id=htmlspecialchars($_GET['id']);
+                //Traitement des données
+                $matchingData=getUsager($id);
+            } else {
+                //Appel de la fonction de lecture des phrases
+                $matchingData=getAllUsager();
+            }
+            //Envoi de la réponse
+            if (empty($matchingData)){
+                deliver_response(404, "Not Found");
+            } else {
+                deliver_response(200, "OK", $matchingData);
+            }
+
+        break;
+        case "POST" :
+            // Récupération des données dans le corps
+            $postedData = file_get_contents('php://input');
+            $data = json_decode($postedData,true); //Reçoit du json et renvoi une adaptation exploitable en php. Le paramètre true impose un tableau en retour et non un objet.
+            
+            //Traitement des données
+            $matchingData=addUsager($data);
+
+            deliver_response(201, "Created", $matchingData);
+
+        break;
+        case "PATCH" :
+            // Récupération des données dans le corps
+            $postedData = file_get_contents('php://input');
+            $data_init = getUsager($_GET['id']);
+            $data = json_decode($postedData,true); //Reçoit du json et renvoi une adaptation exploitable en php. Le paramètre true impose un tableau en retour et non un objet.
+            
+            foreach($data as $key => $value){
+                if($value==null){
+                    $data_init[$key][$value]=$data[$key][$value];
+                }
+            }
+
+            //Traitement des données
+            $matchingData=updateUsager($_GET['id'],$data_init);
+            if (empty($matchingData)){
+                deliver_response(404, "Not Found");
+            } else {
+                deliver_response(200, "OK", $matchingData);
+            }
+        break;
+        case "PUT" :
+            // Récupération des données dans le corps
+            $postedData = file_get_contents('php://input');
+            $data = json_decode($postedData,true); //Reçoit du json et renvoi une adaptation exploitable en php. Le paramètre true impose un tableau en retour et non un objet.
+            
+            //Traitement des données
+            if (isset($_GET['id'])){
+                $matchingData=updateUsager(,$_GET['id'],$data);
+                if (empty($matchingData)){
+                    deliver_response(404, "Not Found");
+                } else {
+                    deliver_response(200, "OK", $matchingData);
+                }
+            } else {
+                deliver_response(400, "Bad Request");
+            }
+        break;
+        case "DELETE" :
+            //Traitement des données
+            if (isset($_GET['id'])){
+                $matchingData=delUsager($_GET['id']);
+                if (empty($matchingData)){
+                    deliver_response(404, "Not Found");
+                } else {
+                    deliver_response(200, "OK", $matchingData);
+                }
+            } else {
+                deliver_response(400, "Bad Request");
+            }
     }
-
-    function getUsager(int $id){
-        $query = $linkpdo->prepare("SELECT p.*, u.* 
-                                        FROM Personne p, Usager u 
-                                        WHERE p.idPersonne = u.idUsager
-                                        AND u.idUsager = :id");
-        $query->bindParam(':id', $id);
-        $query->execute();
-
-        $matchingData = $query->fetchAll(PDO::FETCH_ASSOC);
-        return $matchingData;
-    }
-
-    function addUsager(Usager $usager) {
-
-        $query = $linkpdo->prepare("INSERT INTO Usager (id_usager, civilite, nom, prenom, sexe, adresse, code_postal, ville, date_nais, lieu_nais, num_secu) 
-            VALUES (:idUsager, :civilite, :nom, :prenom, :sexe, :adresse, :codePostal, ville, :dateNaissance, :lieuNaissance, :NumSecuriteSociale)");
-
-        $id = $usager->getIdUsager();
-        $query->bindParam(':idUsager', $id);
-
-        $civilite = $usager->getCivilite();
-        $query->bindParam(':civilite', $civilite);
-
-        $nom = $usager->getNom();
-        $query->bindParam(':nom', $nom);
-
-        $prenom = $usager->getPrenom();
-        $query->bindParam(':prenom', $prenom);
-
-        $sexe = $usager->getSexe();
-        $query->bindParam(':sexe', $sexe);
-
-        $adresse = $usager->getAdresse();
-        $query->bindParam(':adresse', $adresse);
-
-        $codePostal = $usager->getCodePostal();
-        $query->bindParam(':codePostal', $codePostal);
-
-        $ville = $usager->getVille();
-        $query->bindParam(':ville', $ville);
-
-        $dateNaissance = $usager->getDateNaissance()->format('Y-m-d');
-        $query->bindParam(':dateNaissance', $dateNaissance);
-
-        $lieuNaissance = $usager->getLieuNaissance();
-        $query->bindParam(':lieuNaissance', $lieuNaissance);
-
-        $numSecuriteSociale = $usager->getNumSecuriteSociale();
-        $query->bindParam(':NumSecuriteSociale', $numSecuriteSociale);
-
-        $query->execute();
-
-        $matchingData = $query->fetchAll(PDO::FETCH_ASSOC);
-        return $matchingData;
-    }
-
-    function updateUsager(int $id){
-        $query = $linkpdo->prepare("UPDATE Usager
-            SET civilite = :civilite, nom = :nom, prenom = :prenom, sexe = :sexe, adresse = :adresse, code_postal = :codePostal, ville = :ville, date_nais = :dateNaissance, lieu_nais = :lieuNaissance, num_secu = :NumSecuriteSociale
-            WHERE idUsager = :id");
-
-        $id = $usager->getIdUsager();
-        $query->bindParam(':idUsager', $id);
-
-        $civilite = $usager->getCivilite();
-        $query->bindParam(':civilite', $civilite);
-
-        $nom = $usager->getNom();
-        $query->bindParam(':nom', $nom);
-
-        $prenom = $usager->getPrenom();
-        $query->bindParam(':prenom', $prenom);
-
-        $sexe = $usager->getSexe();
-        $query->bindParam(':sexe', $sexe);
-
-        $adresse = $usager->getAdresse();
-        $query->bindParam(':adresse', $adresse);
-
-        $codePostal = $usager->getCodePostal();
-        $query->bindParam(':codePostal', $codePostal);
-
-        $ville = $usager->getVille();
-        $query->bindParam(':ville', $ville);
-
-        $dateNaissance = $usager->getDateNaissance()->format('Y-m-d');
-        $query->bindParam(':dateNaissance', $dateNaissance);
-
-        $lieuNaissance = $usager->getLieuNaissance();
-        $query->bindParam(':lieuNaissance', $lieuNaissance);
-
-        $numSecuriteSociale = $usager->getNumSecuriteSociale();
-        $query->bindParam(':NumSecuriteSociale', $numSecuriteSociale);
-
-        $query->execute();
-
-        $matchingData = $query->fetchAll(PDO::FETCH_ASSOC);
-        return $matchingData;
-    }
-
-    function delUsager(int $id){
-        $query = $linkpdo->prepare("DELETE FROM Usager WHERE idUsager = :id");
-
-        $query->bindParam(':id', $id);
-        $query->execute();
-
-        $matchingData = $query->fetchAll(PDO::FETCH_ASSOC);
-        return $matchingData;
-    }
-    
 ?>
