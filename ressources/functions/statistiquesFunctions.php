1a69cde20a37e4ee965865d425296153dd929d2e
Nail Lamarti <nail.lamarti@etu.iut-tlse3.fr>
2024-03-27 14:49:27 +0100

    update statistiquesFunctions
    cleaned it

--- ressources/functions/statistiquesFunctions.php
+++ ressources/functions/statistiquesFunctions.php
@@ -12,32 +12,29 @@
         // Si l'id est 1, on récupère les statistiques concernant les usagers
         if ($id == 1) {
 
-            // Requête pour récupérer le nombre d'usagers de moins de 25 ans, distingués par civilité
-            $query = $linkpdo->prepare("SELECT sexe, COUNT(*) as nb_usagers
-                                        FROM Usager
-                                        WHERE date_nais > DATE_SUB(CURDATE(), INTERVAL 25 YEAR)
-                                        GROUP BY sexe;");
+            $query = $linkpdo->prepare("
+                SELECT sexe,
+                       SUM(IF(date_nais > DATE_SUB(CURDATE(), INTERVAL 25 YEAR), 1, 0)) AS usagers_moins_25,
+                       SUM(IF(date_nais BETWEEN DATE_SUB(CURDATE(), INTERVAL 50 YEAR) AND DATE_SUB(CURDATE(), INTERVAL 25 YEAR), 1, 0)) AS usagers_25_50,
+                       SUM(IF(date_nais < DATE_SUB(CURDATE(), INTERVAL 50 YEAR), 1, 0)) AS usagers_plus_50
+                FROM Usager
+                GROUP BY sexe;
+            ");
+            
             $query->execute();
-            $matchingData = $query->fetchAll(PDO::FETCH_ASSOC);
-            $data['usagers_moins_25'] = $matchingData;
+            $rawData = $query->fetchAll(PDO::FETCH_ASSOC);
 
-            // Requête pour récupérer le nombre d'usagers entre 25 et 50 ans, distingués par civilité
-            $query = $linkpdo->prepare("SELECT sexe, COUNT(*) as nb_usagers
-                                        FROM Usager
-                                        WHERE date_nais BETWEEN DATE_SUB(CURDATE(), INTERVAL 50 YEAR) AND DATE_SUB(CURDATE(), INTERVAL 25 YEAR)
-                                        GROUP BY sexe;");
-            $query->execute();
-            $matchingData = $query->fetchAll(PDO::FETCH_ASSOC);
-            $data['usagers_25_50'] = $matchingData;
+            $data = [
+                'usagers_moins_25' => [],
+                'usagers_25_50' => [],
+                'usagers_plus_50' => [],
+            ];
 
-            // Requête pour récupérer le nombre d'usagers de plus de 50 ans, distingués par civilité
-            $query = $linkpdo->prepare("SELECT sexe, COUNT(*) as nb_usagers
-                                        FROM Usager
-                                        WHERE date_nais < DATE_SUB(CURDATE(), INTERVAL 50 YEAR)
-                                        GROUP BY sexe;");
-            $query->execute();
-            $matchingData = $query->fetchAll(PDO::FETCH_ASSOC);
-            $data['usagers_plus_50'] = $matchingData;
+            foreach ($rawData as $row) {
+                $data['usagers_moins_25'][$row['sexe']] = $row['usagers_moins_25'];
+                $data['usagers_25_50'][$row['sexe']] = $row['usagers_25_50'];
+                $data['usagers_plus_50'][$row['sexe']] = $row['usagers_plus_50'];
+            }
 
         // Si l'id est 2, on récupère les statistiques concernant les médecins
         } else if ($id == 2) {
