9449058424f8e3b494d0dbaa289bcc80068522e8
Nail Lamarti <nail.lamarti@etu.iut-tlse3.fr>
2024-03-29 15:06:44 +0100

    update auth
    maybe working

--- ressources/statistiques.php
+++ ressources/statistiques.php
@@ -3,34 +3,41 @@
     require_once './functions/statistiquesFunctions.php';
     require_once './functions/response.php';
 
-    /// Identification du type de méthode HTTP envoyée par le client
-    $http_method = $_SERVER['REQUEST_METHOD'];
 
-    switch ($http_method){
+    // Vérification de la validité du token
+    if (!isset($_SERVER['HTTP_TOKEN']) || !is_valid($_SERVER['HTTP_TOKEN'])) {
 
-        // Si la méthode est GET (récupération de données)
-        case "GET" :
+        /// Identification du type de méthode HTTP envoyée par le client
+        $http_method = $_SERVER['REQUEST_METHOD'];
 
-            //Si l'id est set : on le récupère et récupère le type de statistique correspondant
-            if(isset($_GET['id'])){
+        switch ($http_method){
 
-                //Récupération de l'id
-                $id=htmlspecialchars($_GET['id']);
+            // Si la méthode est GET (récupération de données)
+            case "GET" :
 
-                //Appel de la fonction pour récupérer le type de statistiques correspondant à l'id
-                $matchingData=getStats($id);
-            }
+                //Si l'id est set : on le récupère et récupère le type de statistique correspondant
+                if(isset($_GET['id'])){
 
-            //Envoi de la réponse
-            if (empty($matchingData)){
-                deliver_response(404, "Not Found");
-            } else {
-                deliver_response(200, "OK", $matchingData);
-            }
+                    //Récupération de l'id
+                    $id=htmlspecialchars($_GET['id']);
 
-            break;
-        default:
-            deliver_response(400, "Bad Request");
-            break;
+                    //Appel de la fonction pour récupérer le type de statistiques correspondant à l'id
+                    $matchingData=getStats($id);
+                }
+
+                //Envoi de la réponse
+                if (empty($matchingData)){
+                    deliver_response(404, "Not Found");
+                } else {
+                    deliver_response(200, "OK", $matchingData);
+                }
+
+                break;
+            default:
+                deliver_response(400, "Bad Request");
+                break;
+        }
+    } else {
+        deliver_response(401, "Unauthorized");
     }
 ?>
